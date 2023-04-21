9bf8a9be95716360a8c1f9ed7b821bc8424d693a
Chungwei Weng <eyvindove@gmail.com>
2023-04-21 10:11:17 +0800

    initialization

---
+++ README.md
@@ -0,0 +1,17 @@
+# Vite + React + TypeScript + ESLint + Prettier
+
+A boilerplate based on Vite, React, TypeScript, ESLint and Prettier.
+
+Find more...
+
+- [Vite](https://vitejs.dev/)
+- [React](https://react.dev/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [ESLint](https://eslint.org/)
+- [Prettier](https://prettier.io/)
+
+## Get started
+
+1. Clone this repo.
+2. Install all dependencies. (uses pnpm as default, you can use others package managers likes npm or yarn)
+3. If you like this boilerplate, don't forget to give a star! :star2:
---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import '@/App.css'
+
+interface ReferenceConfig {
+  id: string
+  label: string
+  link: string
+}
+
+const referenceConfig: ReferenceConfig[] = [
+  {
+    id: 'vite',
+    label: 'Vite',
+    link: 'https://vitejs.dev/',
+  },
+  {
+    id: 'react',
+    label: 'React',
+    link: 'https://react.dev/',
+  },
+  {
+    id: 'typescript',
+    label: 'TypeScript',
+    link: 'https://www.typescriptlang.org/',
+  },
+  {
+    id: 'eslint',
+    label: 'ESLint',
+    link: 'https://eslint.org/',
+  },
+  {
+    id: 'prettier',
+    label: 'Prettier',
+    link: 'https://prettier.io/',
+  },
+]
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='logo-block'>
+        <a
+          href='https://vitejs.dev'
+          target='_blank'
+        >
+          <img
+            src={viteLogo}
+            className='logo'
+            alt='Vite logo'
+          />
+        </a>
+        <div>X</div>
+        <a
+          href='https://react.dev'
+          target='_blank'
+        >
+          <img
+            src={reactLogo}
+            className='logo react'
+            alt='React logo'
+          />
+        </a>
+      </div>
+      <h1>Vite + React + TypeScript + Eslint + Prettier</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+      <div className='reference-block'>
+        {referenceConfig.map((item: ReferenceConfig) => (
+          <a
+            key={item.id}
+            href={item.link}
+            target='_blank'
+          >
+            {item.label}
+          </a>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
